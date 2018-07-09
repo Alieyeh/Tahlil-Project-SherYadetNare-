9a4060092c6a27b9dbe4e79936e959cdd0e721a6
nazaninsarrafzade <ofoq41@yahoo.com>
2018-07-09 20:18:04 +0430

    files for each table of database is updated

--- java/com/example/alieyeh/appy/FileManager.java
+++ java/com/example/alieyeh/appy/FileManager.java
@@ -1,4 +1,4 @@
-package com.example.nazanin_sarrafzadeh.sheroyadete;
+package com.example.alieyeh.appy;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.*;
 
 /**
  * Created by nazanin-sarrafzadeh on 6/8/2018.
@@ -14,13 +15,59 @@ import java.util.List;
 public class FileManager {
 
     StartingScreenActivity mainActivity = new StartingScreenActivity();
+    static int popCount=35,sonatiCount=18,khazCount=11,titrajCount=7;
 
 
-    public ArrayList<byte[]> listSoundFilesBlob() {
+    public ArrayList<byte[]> listKhazSoundFilesBlob() {
 
         ArrayList<byte[]> soundfiles = new ArrayList<>();
-        for (int i = 0; i < listRawMediaFiles().size(); i++) {
-            InputStream istream = mainActivity.context.getResources().openRawResource(listRawMediaFiles().get(i));
+        for (int i = 0; i < listKhazRawMediaFiles().size(); i++) {
+            InputStream istream = mainActivity.context.getResources().openRawResource(listKhazRawMediaFiles().get(i));
+            try {
+                byte[] music = new byte[istream.available()];
+                istream.read(music);
+                soundfiles.add(music);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return soundfiles;
+    }
+    public ArrayList<byte[]> listPopSoundFilesBlob() {
+
+        ArrayList<byte[]> soundfiles = new ArrayList<>();
+        for (int i = 0; i < listPopRawMediaFiles().size(); i++) {
+            InputStream istream = mainActivity.context.getResources().openRawResource(listPopRawMediaFiles().get(i));
+            try {
+                byte[] music = new byte[istream.available()];
+                istream.read(music);
+                soundfiles.add(music);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return soundfiles;
+    }
+    public ArrayList<byte[]> listSonatiSoundFilesBlob() {
+
+        ArrayList<byte[]> soundfiles = new ArrayList<>();
+        for (int i = 0; i < listSonatiRawMediaFiles().size(); i++) {
+            InputStream istream = mainActivity.context.getResources().openRawResource(listSonatiRawMediaFiles().get(i));
+            try {
+                byte[] music = new byte[istream.available()];
+                istream.read(music);
+                soundfiles.add(music);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return soundfiles;
+    }
+    public ArrayList<byte[]> listTitrajSoundFilesBlob() {
+
+        ArrayList<byte[]> soundfiles = new ArrayList<>();
+        for (int i = 0; i < listTitrajRawMediaFiles().size(); i++) {
+            InputStream istream = mainActivity.context.getResources().openRawResource(listTitrajRawMediaFiles().get(i));
             try {
                 byte[] music = new byte[istream.available()];
                 istream.read(music);
@@ -33,7 +80,43 @@ public class FileManager {
     }
 
     //returns id of songs in asset folder
-    public static List<Integer> listRawMediaFiles() {
+    public static List<Integer> listKhazRawMediaFiles() {
+        List<Integer> ids = new ArrayList<>();
+        //what's your problem
+        for (Field field : R.raw.class.getFields()) {
+            try {
+                ids.add(field.getInt(field));
+            } catch (Exception e) {
+
+            }
+        }
+        return ids;
+    }
+    public static List<Integer> listPopRawMediaFiles() {
+        List<Integer> ids = new ArrayList<>();
+        //what's your problem
+        for (Field field : R.raw.class.getFields()) {
+            try {
+                ids.add(field.getInt(field));
+            } catch (Exception e) {
+
+            }
+        }
+        return ids;
+    }
+    public static List<Integer> listSonatiRawMediaFiles() {
+        List<Integer> ids = new ArrayList<>();
+        //what's your problem
+        for (Field field : R.raw.class.getFields()) {
+            try {
+                ids.add(field.getInt(field));
+            } catch (Exception e) {
+
+            }
+        }
+        return ids;
+    }
+    public static List<Integer> listTitrajRawMediaFiles() {
         List<Integer> ids = new ArrayList<>();
         //what's your problem
         for (Field field : R.raw.class.getFields()) {
@@ -46,59 +129,26 @@ public class FileManager {
         return ids;
     }
 
-    public ArrayList<String> listTextFiles() {
+/////////////////////////////////////
+    public ArrayList<String> listPopTextFiles() {
+
         ArrayList<String> files = new ArrayList<>();
 
         BufferedReader reader = null;
-     //   for (String assetFileName : assetFileNames) {
-            try {
-                reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("texts.txt")));
-
-                String line = "";
-                while ((line = reader.readLine()) != null) {
-
-                    files.add(line);
-
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-
-            }
-            finally {
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-  //      }
-        return files;
-    }
-
-    public ArrayList<String> listSingerNameFiles() {
-        ArrayList<String> files = new ArrayList<>();
-
-        BufferedReader reader = null;
-        //   for (String assetFileName : assetFileNames) {
         try {
-            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("singer_name.txt")));
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("texts_pop.txt")));
 
             String line = "";
             while ((line = reader.readLine()) != null) {
 
                 files.add(line);
+                //popCount++;
 
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
 
-        }
-        finally {
+        } finally {
             if (reader != null) {
                 try {
                     reader.close();
@@ -112,13 +162,12 @@ public class FileManager {
         return files;
     }
 
-    public ArrayList<String> listSongNameFiles() {
+    public ArrayList<String> listPopSingerNameFiles() {
         ArrayList<String> files = new ArrayList<>();
 
         BufferedReader reader = null;
-        //   for (String assetFileName : assetFileNames) {
         try {
-            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("song_name.txt")));
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("singer_name_pop.txt")));
 
             String line = "";
             while ((line = reader.readLine()) != null) {
@@ -126,12 +175,311 @@ public class FileManager {
                 files.add(line);
 
             }
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
 
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        finally {
+
+        return files;
+    }
+
+    public ArrayList<String> listPopSongNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("song_name_pop.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return files;
+    }
+
+
+    /////////////////////////////////////////
+    public ArrayList<String> listSonatiTextFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("texts_sonati.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+                //sonatiCount++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //      }
+        return files;
+    }
+
+    public ArrayList<String> listSonatiSingerNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("singer_name_sonati.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //      }
+        return files;
+    }
+
+    public ArrayList<String> listSonatiSongNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("song_name_sonati.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //      }
+        return files;
+    }
+
+    ////////////////////////////////
+    public ArrayList<String> listTitrajTextFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("texts_titraj.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+                //titrajCount++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return files;
+    }
+
+    public ArrayList<String> listTitrajSingerNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("singer_name_titraj.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //      }
+        return files;
+    }
+
+    public ArrayList<String> listTitrajSongNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("song_name_titraj.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return files;
+    }
+
+    ////////////////////////////////////////////////
+    public ArrayList<String> listKhazTextFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("texts_khaz.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+                //khazCount++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return files;
+    }
+
+    public ArrayList<String> listKhazSingerNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("singer_name_khaz.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return files;
+    }
+
+    public ArrayList<String> listKhazSongNameFiles() {
+        ArrayList<String> files = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(mainActivity.context.getAssets().open("song_name_khaz.txt")));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+
+                files.add(line);
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        } finally {
             if (reader != null) {
                 try {
                     reader.close();
